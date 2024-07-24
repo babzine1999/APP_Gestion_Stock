b5c00e058b4be35517c2064860ff88d31bce0201
babzine1999 <76172597+babzine1999@users.noreply.github.com>
2024-07-24 12:12:47 +0100

    Update Register.js

--- src/Components/Account/Register.js
+++ src/Components/Account/Register.js
@@ -15,7 +15,7 @@ const Register = () => {
         e.preventDefault();
 
         try {
-            const response = await axios.post('https://192.168.1.249:5000/api/Account/register', {
+            const response = await axios.post('https://localhost:7230/api/Account/register', {
                 username,
                 email,
                 password,
